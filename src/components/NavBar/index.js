fa8f5aa6823155113a7611bc7b80597072f56f99
DatSpecialOne <32124297+DatSpecialOne@users.noreply.github.com>
2021-05-21 20:09:28 +0100

    Prototype of clean navigation bar
    Todo:
    *functionality
    *fix scaling issue with

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,33 +1,44 @@
 import React from 'react';
-//import { NavLink } from 'react-router-dom';
+import "../../App.css";
+import{RiAccountCircleLine} from "react-icons/ri"
+import{RiShoppingBasketLine} from "react-icons/ri"
 
 const NavBar = () => {
     return (
-        <div><h1>NavBar</h1></div>
-        // <>
-        //     <Nav>
-        //         <NavLink to="/">
-        //             <h1>LOGO</h1>
-        //         </NavLink>
-        //         <NavLink to="/">
-        //             <h1>SearchBar</h1>
-        //         </NavLink>
-        //         <NavLink to="/">
-        //             <h1>account</h1>
-        //         </NavLink>
-        //         <NavLink to="/">
-        //             <h1>basket</h1>
-        //         </NavLink>
-
-
-        //         <Bars />
-        //         <NavMenu>
-        //             <NavLink to="/" activeStyle>
-
-        //             </NavLink>
-        //         </NavMenu>
-        //     </Nav>
-        // </>
+        //<div><h1>NavBar</h1></div>
+        <div className="NavBar">
+            <div className="MainNav">
+                <div className="Logo">
+                    <h1>LOGO</h1>
+                </div>
+                <div className="SearchBar">
+                    <input type="text" placeholder="Seach..." className="Form"/>
+                </div>
+                <div className="Icon">
+                    <button className="IconBtn">
+                        <h1>
+                            <RiAccountCircleLine/>
+                        </h1>
+                    </button>
+                </div>
+                <div className="Icon">
+                    <button className="IconBtn">
+                        <h1>
+                            <RiShoppingBasketLine/>
+                        </h1>
+                    </button>
+                </div>
+            </div>
+            <div className="SubNav">
+            <div className="NavCategory">
+                    <button className="TextBtn">
+                        <h1>
+                            Dog
+                        </h1>
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 }
 
