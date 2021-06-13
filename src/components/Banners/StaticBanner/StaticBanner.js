26badbf567a89d710c5acefebf515c4d94c8b45f
DatSpecialOne <32124297+DatSpecialOne@users.noreply.github.com>
2021-06-13 21:26:20 +0100

    Banner complete
    Static Banner with animated button

--- src/components/Banners/StaticBanner/StaticBanner.js
+++ src/components/Banners/StaticBanner/StaticBanner.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Container, Wrapper, BannerBtn, BannerImg } from './StaticBannerElem';
-import BlingBling from './../TestImages/BlingBling.jpg';
+import { Container, Wrapper, BannerBtn, BannerImg, BannerTitle, BannerLink } from './StaticBannerElem';
+import dogs from './../TestImages/dogs.png';
 
 const StaticBanner = () => {
     return (
         <Container>
             <Wrapper>
-                <BannerImg to='/Banner'>Image Here</BannerImg>
+                <BannerLink to='/BannerLink'>
+                    <BannerImg src ={dogs}/>
+                    <BannerTitle>Banner Title</BannerTitle>
+                    <BannerBtn to='/BannerLink'>Banner Button</BannerBtn>
+                </BannerLink>
             </Wrapper>
-            
         </Container>
     );
-
 };
 
-export default StaticBanner
+export default StaticBanner;
--- src/components/Banners/StaticBanner/StaticBannerElem.js
+++ src/components/Banners/StaticBanner/StaticBannerElem.js
@@ -1,21 +1,75 @@
 import styled from 'styled-components';
-import {NavLink as Link} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
     padding: 0px 140px;
     background-color: none;
-`
+`;
 
 export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     marign: 0 auto;
-    max-width: 1500px;    
-`
-export const BannerImg = styled(Link)`
-    
+    max-width: 1600px;
+    position: relative;    
+`;
+
+
+export const BannerImg = styled.img`
+    height: 700px;
+    width: 1600px;
+`;
+
+export const BannerTitle = styled.div`
+    background-color: white;
+    display: flex;
+    height: 100px;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    padding: 0px 10px;
+    width: max-content;   
+    font-weight: bold;
+    font-size: 50px;
+    position: absolute;
+    bottom: 170px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+
+export const BannerLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+    border: none;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+        color: black;
+    }
 `
 
-export const BannerBtn = styled(Link)`
+export const BannerBtn = styled(NavLink)`
+    background-color: white;
+    text-decoration: none;
+    color: black;
+    border: none;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    padding: 0px 20px;
+    position: absolute;
+    bottom: 70px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-weight: bold;
+    transition: 1s;
 
-`
+    &:hover {
+       color: white;
+       background-color: black;
+       text-decoration: none;
+    }
+`;
