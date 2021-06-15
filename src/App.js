603b85b0adf35c6692baf8b196baa3fa5f7a28e5
DatSpecialOne <32124297+DatSpecialOne@users.noreply.github.com>
2021-06-15 16:38:16 +0100

    Footer and Nav Bar at top

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ContainerFoot } from './components/Footer/FooterContainer';
+import NavigationBar from './components/NavBar/NavigationBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <ContainerFoot/>
+      <NavigationBar class='Nav'/>
+      <h4>HELLO</h4>
+      <ContainerFoot class='Footer'/>
     </BrowserRouter>
 
   );
